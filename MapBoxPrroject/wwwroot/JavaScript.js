d6e3792d96d7937bb02456df9964f9ac89fa68d0
Rakesh Kumar <gpaswan1976@gmail.com>
2023-05-02 12:30:32 +0530

    added earnings page

--- MapBoxPrroject/wwwroot/JavaScript.js
+++ MapBoxPrroject/wwwroot/JavaScript.js
@@ -24,6 +24,47 @@ function initializeMap() {
         'top-left'
     );
 
+    map.addControl(new mapboxgl.NavigationControl());
+
+    // Make a request to the Mapbox Directions API to retrieve cycling directions
+    var url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/-122.42,37.78-77.03,38.91?access_token='+ mapboxgl.accessToken;
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Add the route to the map
+            map.addLayer({
+                id: 'route',
+                type: 'line',
+                source: {
+                    type: 'geojson',
+                    data: {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                            type: 'LineString',
+                            coordinates: data.routes[0].geometry.coordinates
+                        }
+                    }
+                },
+                layout: {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                paint: {
+                    'line-color': '#3887be',
+                    'line-width': 5,
+                    'line-opacity': 0.75
+                }
+            });
+
+            // Zoom the map to fit the route
+            var bounds = data.routes[0].geometry.coordinates.reduce(function (bounds, coord) {
+                return bounds.extend(coord);
+            }, new mapboxgl.LngLatBounds(data.routes[0].geometry.coordinates[0], data.routes[0].geometry.coordinates[0]));
+            map.fitBounds(bounds, { padding: 50 });
+        });
+
 
     // Add a marker at the initial location
     // Create a new marker.
